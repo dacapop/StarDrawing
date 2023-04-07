3a3cf762f631b65f47bdd543d4819235d2f78c6b
Ponlakrit.T <30840642+dacapop@users.noreply.github.com>
2023-04-08 01:12:14 +0700

    Add files via upload

---
+++ V1/StarDrawing/StarDrawing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDrawing
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int iLineNum = 0;
+
+            Console.WriteLine("Enter total line number : ");
+            try
+            {
+                iLineNum = Convert.ToInt16(Console.ReadLine());
+
+                if(iLineNum < 5)
+                {
+                    Console.WriteLine("total line number cannot less than 5");
+                }
+                else if(iLineNum%2 == 0)
+                {
+                    Console.WriteLine("total line number cannot be an even number");
+                }
+                else
+                {
+                    Star myStar = new Star(iLineNum);
+                    myStar.DrawMyStar();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exit with error : " + ex.Message);
+            }
+        }
+    }
+}
---
+++ V1/StarDrawing/StarDrawing/Star.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDrawing
+{
+    internal class Star
+    {
+        private int iMaxLine = 0;
+
+        public Star(int maxLine)
+        {
+            this.iMaxLine = maxLine;
+        }
+
+        public void DrawMyStar()
+        {
+            for (int i=1; i<=iMaxLine; i++)
+            {
+                ArrayList alRes = GetStarPosition(i, iMaxLine);
+
+                for(int j=1; j<=((2*iMaxLine)+1); j++)
+                {
+                    if (alRes.Contains(j))
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        public ArrayList GetStarPosition(int iLineNum, int iMaxLine)
+        {
+            ArrayList alResult = new ArrayList();
+            double dMedian = Math.Ceiling(Convert.ToDouble(iMaxLine) / 2);
+
+            int v1 = (iMaxLine + 2) - iLineNum;
+            alResult.Add(v1);
+
+            if (iLineNum > 1)
+            {
+                int v2 = v1 + (2 * (iLineNum - 1));
+                alResult.Add(v2);
+
+                if (iLineNum == dMedian)
+                {
+                    alResult.Add(1);
+                    alResult.Add((2 * iMaxLine) + 1);
+                }
+                else if(iLineNum == dMedian + 1)
+                {
+                    alResult.Add(iMaxLine+1);
+                }   
+            }
+
+            return (alResult);
+        }
+    }
+}
